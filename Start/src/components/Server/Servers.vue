815e1569201745cfb30a7a7e998c9607af6e5744
C0DERAI <31708218+C0DERAI@users.noreply.github.com>
2018-03-28 23:59:41 +0200

    Add files via upload

---
+++ Start/src/components/Server/Servers.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <!-- <li
+                    class="list-group-item"
+                    v-for="index in 5">
+                Server #{{ index }}
+            </li> -->
+            <li class="list-group-item"  v-for="server in servers"> <a href="#"> ID:{{ server.id }} Status:{{ server.status }}</a> </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+// import { busEvent } from '../main';
+
+export default{
+    data: function(){
+        return{
+            servers: [
+                {id: 1, status: 'Critical'},
+                {id: 2, status: 'Normal'},
+                {id: 3, status: 'Unknown'},
+                {id: 4, status: 'Normal'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
